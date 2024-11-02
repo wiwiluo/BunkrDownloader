5228977258764c98645d0107da59e2c2fe604f53
Lysagxra <g0ld3nd34d@gmail.com>
2024-11-02 16:59:48 +0100

    Add files via upload

---
+++ helpers/progress_utils.py
@@ -0,0 +1,107 @@
+"""
+This module provides utility functions for tracking download progress and
+displaying logs using the Rich library. It includes features for creating a
+progress bar, a formatted progress table for monitoring download status, and
+a log table for displaying downloaded messages.
+"""
+
+from rich.table import Table
+from rich.panel import Panel
+from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    BarColumn,
+    TextColumn
+)
+
+TITLE_COLOR = "light_cyan3"
+
+def truncate_description(description, max_length=8):
+    """
+    Truncates a string to a specified maximum length, adding an ellipsis if
+    truncated.
+
+    Args:
+        description (str): The string to be truncated.
+        max_length (int): The maximum length of the returned string. 
+                          Default is 8.
+
+    Returns:
+        str: The original string if its length is less than or equal to 
+             `max_length`, otherwise the truncated string with an ellipsis.
+    """
+    return description[:max_length] + "..." if len(description) > max_length \
+        else description
+
+def create_progress_bar():
+    """
+    Creates and returns a progress bar for tracking download progress.
+
+    Returns:
+        Progress: A Progress object configured with relevant columns.
+    """
+    return Progress(
+        "{task.description}",
+        SpinnerColumn(),
+        BarColumn(),
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%")
+    )
+
+def create_progress_table(overall_progress, job_progress):
+    """
+    Creates a formatted progress table for tracking the download.
+
+    Parameters:
+        overall_progress (str): A string representing the overall progress of
+                                the download.
+        job_progress (Progress): An instance of a progress tracking object that 
+                                 manages the download progress of individual
+                                 files.
+
+    Returns:
+        Table: A rich Table object containing a grid layout with two panels:
+               one for overall progress and another for job-specific progress, 
+               each with styled titles and borders.
+    """
+    progress_table = Table.grid()
+    progress_table.add_row(
+        Panel.fit(
+            overall_progress,
+            title=f"[b {TITLE_COLOR}]Overall Progress",
+            border_style="bright_blue",
+            padding=(1, 1),
+            width=40
+        ),
+        Panel.fit(
+            job_progress,
+            title=f"[b {TITLE_COLOR}]Album Progress",
+            border_style="medium_purple",
+            padding=(1, 1),
+            width=40
+        ),
+    )
+    return progress_table
+
+def create_log_table(log_messages):
+    """
+    Creates a formatted log table to display downloaded messages.
+
+    Parameters:
+        log_messages (Progress): An instance of a progress tracking object.
+
+    Returns:
+        Table: A rich Table object containing the formatted log panel with 
+               the specified log messages.
+    """
+    log_row = "\n".join([f"• {message}" for message in log_messages])
+    log_table = Table.grid()
+    log_table.add_row(
+        Panel(
+            log_row,
+            title=f"[b {TITLE_COLOR}]Log Messages",
+            border_style="grey35",
+            padding=(1, 1),
+            width=80
+        )
+    )
+    return log_table
