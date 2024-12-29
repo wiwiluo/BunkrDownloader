3fe36a5104ce82423393aa2036dcb07d6a2a042e
Lysagxra <g0ld3nd34d@gmail.com>
2024-12-29 12:48:42 +0100

    Update log_manager.py

--- helpers/managers/log_manager.py
+++ helpers/managers/log_manager.py
@@ -10,11 +10,6 @@ the number of rows to display and the style of the borders and headers.
 This module can be integrated into a live display using the `rich.live.Live`
 and `rich.console.Group` to combine the logger table with other content,
 like progress indicators.
-
-Example usage:
-    - Instantiate a `LoggerTable` object.
-    - Log events with `log(event, details)`.
-    - Use `render_table()` to get the current table representation for display.
 """
 
 from collections import deque
@@ -31,10 +26,12 @@ class LoggerTable:
 
     Args:
         max_rows (int): The maximum number of rows to display in the table.
-                        Older entries will be discarded when the buffer exceeds
+                        Older entries are removed when the buffer exceeds
                         this size. Default is 4.
+        title_color (str): The color used for the table title.
+                           Default is "light_cyan3".
         border_style (str): The style of the table's border and column headers.
-                            Default is "dim".
+                            Default is "cyan".
     """
 
     def __init__(
@@ -48,15 +45,30 @@ class LoggerTable:
         self.border_style = border_style
         self.table = self._create_table()
 
+    def log(self, event, details):
+        """Add a new row to the table and manage scrolling."""
+        timestamp = datetime.now().strftime("%H:%M:%S")
+        self.row_buffer.append((timestamp, event, details))
+
+    def render_log_panel(self):
+        """Renders the log panel containing the log table."""
+        log_table = self._render_table()
+        return Panel(
+            log_table,
+            title=f"[bold {self.title_color}]Log Messages",
+            border_style=self.border_style
+        )
+
+    # Private methods
     def _create_table(self):
         """
         Create and return a new table with the necessary columns and styles.
         """
         new_table = Table(
-            box=SIMPLE,
-            show_header=True,
-            show_edge=True,
-            show_lines=False,
+            box=SIMPLE,                    # Box style for the table
+            show_header=True,              # Show the table column names
+            show_edge=True,                # Display edges around the table
+            show_lines=False,              # Do not display grid lines
             border_style=self.title_color  # Set the color of the box
         )
         new_table.add_column(
@@ -68,37 +80,15 @@ class LoggerTable:
         new_table.add_column(
             f"[{self.title_color}]Details", style="pale_turquoise4", width=45
         )
-
         return new_table
 
-    def log(self, event: str, details: str):
-        """
-        Add a new row to the table and manage scrolling.
-
-        Args:
-            event (str): The type of event.
-            details (str): The event details.
-        """
-        timestamp = datetime.now().strftime("%H:%M:%S")
-        self.row_buffer.append((timestamp, event, details))
-
-    def render_table(self):
+    def _render_table(self):
         """Render the logger table with the current buffer contents."""
-        # Create a new table and populate it with the row buffer contents
+        # Create a new table
         new_table = self._create_table()
 
+        # Populate the new table with the row buffer contents
         for row in self.row_buffer:
             new_table.add_row(*row)
 
         return new_table
-
-    def render_log_panel(self):
-        """
-        Renders the log panel containing the log table.
-        """
-        log_table = self.render_table()
-        return Panel(
-            log_table,
-            title=f"[bold {self.title_color}]Log Messages",
-            border_style=self.border_style
-        )
