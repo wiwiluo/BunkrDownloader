3e2cc9920d6356a0685ec75271c874488c91ae04
Lysagxra <g0ld3nd34d@gmail.com>
2025-01-11 00:26:01 +0100

    Update crawler_utils.py

--- helpers/crawlers/crawler_utils.py
+++ helpers/crawlers/crawler_utils.py
@@ -1,44 +1,9 @@
 """
 Module for extracting media download links from item pages.
-
-This module contains functions that interact with parsed HTML content to
-extract media download links (e.g., videos or images) from item pages. It
-utilizes both Playwright for dynamic content extraction and BeautifulSoup for
-static content scraping.
 """
 
 from helpers.general_utils import fetch_page
-from helpers.url_utils import (
-    get_item_type,
-    get_item_filename,
-    validate_item_page
-)
-
-from .playwright_crawler import extract_media_download_link
-
-async def extract_with_playwright(url):
-    """
-    Initiates the download process for the specified URL using Playwright.
-
-    Args:
-        url (str): The URL of the media to download.
-
-    Returns:
-        str or None: The download link if successful, or None if an error
-                     occurs.
-    """
-    item_type = get_item_type(url)
-    media_type_mapping = {'v': 'video', 'i': 'picture'}
-
-    if item_type not in media_type_mapping:
-        print(
-            f"Unknown item type: {item_type}. "
-            f"Supported types are: {list(media_type_mapping.keys())}."
-        )
-        return None
-
-    media_type = media_type_mapping[item_type]
-    return await extract_media_download_link(url, media_type)
+from helpers.url_utils import get_item_filename
 
 def extract_item_pages(soup, host_page):
     """
@@ -101,6 +66,8 @@ def get_item_container(item_soup):
             }
         )
 
+    # Return the found element (either <source>, <img>, or None if neither was
+    # found)
     return item_container
 
 async def get_non_media_download_link(item_soup):
@@ -138,7 +105,7 @@ async def get_non_media_download_link(item_soup):
     non_media_item_soup = await fetch_page(non_media_container['href'])
 
     # Find the second download button in the fetched page
-    non_media_item_download_container = non_media_item_soup.find(
+    non_media_download_container = non_media_item_soup.find(
         'a',
         {
             'class': (
@@ -150,7 +117,7 @@ async def get_non_media_download_link(item_soup):
     )
 
     # Return the download link extraced from the 'href' attribute
-    return non_media_item_download_container['href']
+    return non_media_download_container['href']
 
 async def get_item_download_link(item_soup):
     """
@@ -181,27 +148,20 @@ async def get_item_download_link(item_soup):
 
     return None
 
-async def get_download_info(item_soup, item_page):
+async def get_download_info(item_soup):
     """
     Gathers download information (link and filename) for the item.
 
     Args:
         item_soup (BeautifulSoup): Parsed HTML content of the item.
-        item_page (str): The item page URL.
 
     Returns:
         tuple: A tuple containing the download link and file name.
     """
-    validated_item_page = validate_item_page(item_page)
-    if item_soup is None:
-        return await extract_with_playwright(validated_item_page)
-
-#    item_type = get_item_type(validated_item_page)
     item_download_link = await get_item_download_link(item_soup)
 
     item_file_name = (
         get_item_filename(item_download_link) if item_download_link
         else None
     )
-
     return item_download_link, item_file_name
