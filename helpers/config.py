b9518e2bc740ee73fc882d531958b970e93b4432
Lysagxra <g0ld3nd34d@gmail.com>
2025-01-11 00:26:41 +0100

    Update config.py

--- helpers/config.py
+++ helpers/config.py
@@ -10,7 +10,7 @@ DOWNLOAD_FOLDER = "Downloads"             # The folder where downloaded files
                                           # will be stored.
 FILE = "URLs.txt"                         # The name of the file containing the
                                           # list of URLs to process.
-SESSION_LOG = "session_log.txt"           # The file used to log session errors.
+SESSION_LOG = "session_log.txt"           # The file used to log errors.
 
 # Headers used for general HTTP requests, mimicking a browser user agent.
 HEADERS = {
@@ -22,22 +22,7 @@ HEADERS = {
 
 # Headers specifically tailored for download requests.
 DOWNLOAD_HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) "
-        "Gecko/20100101 Firefox/117.0"
-    ),
+    **HEADERS,
     "Connection": "keep-alive",
     "Referer": "https://get.bunkrr.su/"
-}
-
-# Configuration for the browser automation module.
-BROWSER_CONFIG = {
-    "url": "https://9xbuddy.in/en",
-    "input_selector": "input.w-full",
-    "button_selector": r".w-8\/12",
-    "download_button": (
-        r"div.lg\:flex:nth-child(6) > div:nth-child(2) > a:nth-child(1)"
-    ),
-    "attribute": "href",
-    "button_timeout": 5000
 }
