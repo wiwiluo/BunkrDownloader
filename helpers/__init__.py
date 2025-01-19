492c488b1d2af68d941098cd96b3705e443f0744
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-01-19 17:47:03 +0100

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,9 +4,8 @@ the main application. These utilities include functions for downloading,
 file management, URL handling, progress tracking, and more.
 
 Modules:
-    - config: Constants and settings used across the project.
     - bunkr_utils: Functions for checking Bunkr status and URL validation.
-    - download_utils: Functions for handling downloads.
+    - config: Constants and settings used across the project.
     - file_utils: Utilities for managing file operations.
     - general_utils: Miscellaneous utility functions.
     - url_utils: Utilities to analyze and extract details from URLs.
@@ -18,9 +17,8 @@ to be easily imported and used across different parts of the application.
 # helpers/__init__.py
 
 __all__ = [
-    "config",
     "bunkr_utils",
-    "download_utils",
+    "config",
     "file_utils",
     "general_utils",
     "url_utils"
