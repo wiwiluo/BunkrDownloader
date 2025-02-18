4bfb6869cc6fa889066629b88309a2faca36a521
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-02-18 20:50:01 +0100

    Refactor code for Ruff compliance
    Updated codebase to follow Ruff linter's recommendations and formatting rules. 
    Refactored various sections to improve readability and ensure consistent styling.

--- downloader.py
+++ downloader.py
@@ -1,65 +1,55 @@
-"""
-Python-based downloader for Bunkr albums and files, supporting single-file and
-album downloads with error logging.
+"""Python-based downloader for Bunkr albums and files.
 
 Usage:
     Run the script from the command line with a valid album or media URL:
         python3 downloader.py <album_or_media_url>
 """
 
-import sys
-import asyncio
+from __future__ import annotations
+
 import argparse
+import asyncio
+import sys
+from argparse import Namespace
 
 from requests.exceptions import (
     ConnectionError as RequestConnectionError,
-    Timeout, RequestException
 )
-
-from helpers.managers.live_manager import LiveManager
-from helpers.managers.log_manager import LoggerTable
-from helpers.managers.progress_manager import ProgressManager
-
-from helpers.downloaders.album_downloader import AlbumDownloader
-from helpers.downloaders.album_downloader import MediaDownloader
-
-from helpers.crawlers.crawler_utils import (
-    extract_item_pages,
-    get_download_info
+from requests.exceptions import (
+    RequestException,
+    Timeout,
 )
 
 from helpers.bunkr_utils import get_bunkr_status
+from helpers.crawlers.crawler_utils import extract_item_pages, get_download_info
+from helpers.downloaders.album_downloader import AlbumDownloader, MediaDownloader
 from helpers.general_utils import (
-    fetch_page,
-    create_download_directory,
     clear_terminal,
-    format_directory_name
+    create_download_directory,
+    fetch_page,
+    format_directory_name,
 )
+from helpers.managers.live_manager import LiveManager
+from helpers.managers.log_manager import LoggerTable
+from helpers.managers.progress_manager import ProgressManager
 from helpers.url_utils import (
     check_url_type,
-    get_identifier,
-    get_album_name,
     get_album_id,
+    get_album_name,
     get_host_page,
+    get_identifier,
 )
 
-async def handle_download_process(
-    bunkr_status, page_info, download_path, live_manager, args
-):
-    """
-    Handles the download process for a Bunkr album or a single item.
 
-    Args:
-        bunkr_status (dict): Current status of Bunkr subdomains.
-        page_info (tuple): Contains details of the page to process:
-            - url (str): The URL of the item or album to download.
-            - soup (BeautifulSoup): Parsed HTML content of the page.
-        download_path (str): The directory path where downloaded files will be
-                             saved.
-        live_manager (LiveManager): Manages the live display and updates
-                                    during the download process.
-    """
-    (url, soup) = page_info
+async def handle_download_process(
+    bunkr_status: dict,
+    page_info: tuple,
+    download_path: str,
+    live_manager: LiveManager,
+    args: Namespace,
+) -> None:
+    """Handle the download process for a Bunkr album or a single item."""
+    url, soup = page_info
     host_page = get_host_page(url)
     identifier = get_identifier(url)
 
@@ -69,7 +59,7 @@ async def handle_download_process(
             session_info=(bunkr_status, download_path),
             album_info=(identifier, item_pages),
             live_manager=live_manager,
-            args=args
+            args=args,
         )
         await album_downloader.download_album()
 
@@ -85,30 +75,17 @@ async def handle_download_process(
         )
         downloader.download()
 
-async def validate_and_download(bunkr_status, url, live_manager, args=None):
-    """
-    Validates the provided URL, prepares the download directory, and initiates
-    the download process.
 
-    Args:
-        bunkr_status (object): The session status or context used for managing
-                               the download process.
-        url (str): The URL to validate and download content from.
-        live_manager (object): An object for managing live progress updates
-                               and task states.
-
-    Raises:
-        RequestConnectionError: If there is a connection error during the
-                                download process.
-        Timeout: If the request times out while processing the URL.
-        RequestException: If there is a general request-related error.
-    """
+async def validate_and_download(
+        bunkr_status: dict,
+        url: str,
+        live_manager: LiveManager,
+        args: Namespace | None = None,
+) -> None:
+    """Validate the provided URL, and initiate the download process."""
     soup = await fetch_page(url)
 
-    album_id = (
-        get_album_id(url) if check_url_type(url)
-        else None
-    )
+    album_id = get_album_id(url) if check_url_type(url) else None
     album_name = get_album_name(soup)
 
     directory_name = format_directory_name(album_name, album_id)
@@ -116,76 +93,46 @@ async def validate_and_download(bunkr_status, url, live_manager, args=None):
 
     try:
         await handle_download_process(
-            bunkr_status,
-            (url, soup),
-            download_path,
-            live_manager,
-            args=args
+            bunkr_status, (url, soup), download_path, live_manager, args=args,
         )
 
     except (RequestConnectionError, Timeout, RequestException) as err:
-        print(f"Error downloading from {url}: {err}")
+        error_message = f"Error downloading from {url}: {err}"
+        raise RuntimeError(error_message) from err
 
-def initialize_managers():
-    """
-    Initializes and returns the managers for progress tracking and logging.
 
-    Returns:
-        LiveManager: Handles the live display of progress and logs.
-    """
-    progress_manager = ProgressManager(
-        task_name = "Album",
-        item_description="File"
-    )
+def initialize_managers() -> LiveManager:
+    """Initialize and return the managers for progress tracking and logging."""
+    progress_manager = ProgressManager(task_name="Album", item_description="File")
     logger_table = LoggerTable()
     return LiveManager(progress_manager, logger_table)
 
-def parse_arguments():
-    """
-    Parses command-line arguments for the URL, an optional ignore list, and an
-    optional include list.
 
-    Returns:
-        Namespace: Parsed arguments, including:
-            - url (str): The URL to process (required).
-            - ignore_list (list of str): A list of substrings; files whose
-                                         names contain any of these substrings
-                                         will be skipped (optional).
-            - include_list (list of str): A list of substrings; files whose
-                                         names contain any of these substrings
-                                         will only be downloaded (optional).
-    """
-    parser = argparse.ArgumentParser(
-        description='Acquire URL and other arguments.'
+def parse_arguments() -> Namespace:
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description="Acquire URL and other arguments.")
+    parser.add_argument("url", type=str, help="The URL to process")
+    parser.add_argument(
+        "--ignore",
+        type=str,
+        nargs="+",
+        help="A list of substrings to match against filenames. "
+        "Files containing any of these substrings "
+        "in their names will be skipped.",
     )
     parser.add_argument(
-        'url',
+        "--include",
         type=str,
-        help='The URL to process'
+        nargs="+",
+        help="A list of substrings to match against filenames. "
+        "Files containing any of these substrings "
+        "in their names will be downloaded.",
     )
-    parser.add_argument(
-        '--ignore',
-        type=str,
-        nargs='+',
-        help='A list of substrings to match against filenames. '
-             'Files containing any of these substrings '
-             'in their names will be skipped.'
-    )
-    parser.add_argument(
-        '--include',
-        type=str,
-        nargs='+',
-        help='A list of substrings to match against filenames. '
-             'Files containing any of these substrings '
-             'in their names will be downloaded.'
-    )
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
-async def main():
-    """
-    Main function for initiating the download process.
-    """
+
+async def main() -> None:
+    """Initialize the download process."""
     clear_terminal()
     bunkr_status = get_bunkr_status()
     live_manager = initialize_managers()
@@ -193,16 +140,12 @@ async def main():
 
     try:
         with live_manager.live:
-            await validate_and_download(
-                bunkr_status,
-                args.url,
-                live_manager,
-                args=args
-            )
+            await validate_and_download(bunkr_status, args.url, live_manager, args=args)
             live_manager.stop()
 
     except KeyboardInterrupt:
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-"""
-This module provides functionality to read URLs from a specified file,
-validate them, and download the associated content. It manages the entire
-download process by leveraging asynchronous operations, allowing for
-efficient handling of multiple URLs.
+"""Provide functionality to read URLs from a file, and download from them.
+
+This module manages the entire download process by leveraging asynchronous
+operations, allowing for efficient handling of multiple URLs.
 
 Usage:
     To run the module, execute the script directly. It will process URLs
     listed in 'URLs.txt' and log the session activities in 'session_log.txt'.
 """
 
-import sys
 import asyncio
+import sys
 
+from downloader import initialize_managers, validate_and_download
 from helpers.bunkr_utils import get_bunkr_status
+from helpers.config import FILE, SESSION_LOG
 from helpers.file_utils import read_file, write_file
 from helpers.general_utils import clear_terminal
-from helpers.config import FILE, SESSION_LOG
 
-from downloader import validate_and_download, initialize_managers
 
-async def process_urls(urls):
-    """
-    Validates and downloads items for a list of URLs.
-
-    Args:
-        urls (list): A list of URLs to process.
-    """
+async def process_urls(urls: list[str]) -> None:
+    """Validate and downloads items for a list of URLs."""
     bunkr_status = get_bunkr_status()
     live_manager = initialize_managers()
 
@@ -33,15 +27,15 @@ async def process_urls(urls):
         with live_manager.live:
             for url in urls:
                 await validate_and_download(bunkr_status, url, live_manager)
+
             live_manager.stop()
 
     except KeyboardInterrupt:
         sys.exit(1)
 
-async def main():
-    """
-    Main function to execute the script.
-    """
+
+async def main() -> None:
+    """Run the script and process URLs."""
     # Clear the terminal and session log file
     clear_terminal()
     write_file(SESSION_LOG)
@@ -53,5 +47,6 @@ async def main():
     # Clear URLs file
     write_file(FILE)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
