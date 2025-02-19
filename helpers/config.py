af5404320468a9755e4851102714b16d01b1483c
Lysagxra <185969723+Lysagxra@users.noreply.github.com>
2025-02-19 11:10:26 +0100

    Define thresholds and chunk sizes with improved granularity
    - Defined file size thresholds and corresponding chunk sizes for more efficient file downloads.
    - Increased chunk sizes for medium to large files to reduce overhead.
    - Added additional thresholds for better granularity in handling a wider range of file sizes.

--- helpers/config.py
+++ helpers/config.py
@@ -12,15 +12,31 @@ FILE = "URLs.txt"                         # The name of the file containing the
                                           # list of URLs to process.
 SESSION_LOG = "session_log.txt"           # The file used to log errors.
 
-# Constants for file size conversions, expressed in bytes
+# Constants for file sizes, expressed in bytes.
 KB = 1024
 MB = 1024 * KB
+GB = 1024 * MB
 
-# HTTP status codes
+# Thresholds for file sizes and corresponding chunk sizes used during download.
+# Each tuple represents: (file size threshold, chunk size to download in that range).
+THRESHOLDS = [
+    (1 * MB, 32 * KB),    # Less than 1 MB
+    (10 * MB, 128 * KB),  # 1 MB to 10 MB
+    (50 * MB, 512 * KB),  # 10 MB to 50 MB
+    (100 * MB, 1 * MB),   # 50 MB to 100 MB
+    (250 * MB, 2 * MB),   # 100 MB to 250 MB
+    (500 * MB, 4 * MB),   # 250 MB to 500 MB
+    (1 * GB, 8 * MB),     # 500 MB to 1 GB
+]
+
+# Default chunk size for files larger than the largest threshold.
+LARGE_FILE_CHUNK_SIZE = 16 * MB
+
+# HTTP status codes.
 HTTP_STATUS_BAD_GATEWAY = 502
 HTTP_STATUS_SERVER_DOWN = 521
 
-# Headers used for general HTTP requests, mimicking a browser user agent.
+# Headers used for general HTTP requests.
 HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) "
